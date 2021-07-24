98f3b41f2133c2dff16dbfd60196e427a062a488
KentaMatsumura <photosynthesishope@gmail.com>
2021-07-24 21:07:14 +0900

    ユーザ情報からtimeを削除

--- model/user.py
+++ model/user.py
@@ -4,7 +4,7 @@ from datetime import datetime
 class User:
     id = ""
     name = ""
-    timestamp = datetime()
+    # timestamp = datetime()
 
     def __init__(self, id):
         self.id = id
@@ -13,8 +13,8 @@ class User:
     def set_user_name(self, message):
         self.name = message
 
-    def set_user_timestamp(self, timestamp):
-        self.timestamp = datetime(timestamp)
+    # def set_user_timestamp(self, timestamp):
+    #     self.timestamp = datetime(timestamp)
 
     def get_user(self):
         return {'id': self.id, 'name': self.name}
