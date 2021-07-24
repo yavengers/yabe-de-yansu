9e478a693caaea2e49b9f04374d95f5bf6b3f92f
KentaMatsumura <photosynthesishope@gmail.com>
2021-07-24 17:36:57 +0900

    不要なコメントの削除

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def callback():
     signature = request.headers['X-Line-Signature']
 
     # get request body as text
-    body = request.get_data(as_text=True) + "でやんす"
+    body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
     # handle webhook body
