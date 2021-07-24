96c77e3329b93a6bed1ea434f2274b575ec3c98f
KentaMatsumura <photosynthesishope@gmail.com>
2021-07-24 17:39:03 +0900

    語尾の変更

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from types import resolve_bases
 from typing import Text
 from flask import Flask, Blueprint, request, abort
 import os
@@ -49,9 +50,10 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    response_message = event.message.text + "でやんす"
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text)
+        TextSendMessage(text=response_message)
     )
 
 
