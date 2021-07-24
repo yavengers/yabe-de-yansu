8f34b54a8f4072e400a16cf4a09f86687595e9f9
KentaMatsumura <photosynthesishope@gmail.com>
2021-07-24 22:42:54 +0900

    コンテンツの取得の変更

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from typing import Text
 from flask import Flask, request, abort
 import os
 import json
+import requests
 
 
 from linebot import (
@@ -62,20 +63,20 @@ def handle_message(event):
     prev_message_text = ""
     if event.message.text == scenario['rich_menu_scenario']['register_user']['trigger_message']:
         prev_message_id = rm_handler.menu_a()
-        prev_message_text = line_bot_api.get_message_content(
-            str(prev_message_id))
-        print(prev_message_text)
+        prev_message_text = requests.get(
+            'https://api-data.line.me/v2/bot/message/' + prev_message_id + '/content', headers=request_header)
+        print("prev_message" + prev_message_text)
     elif event.message.text == scenario['rich_menu_scenario']['get_time_now']['trigger_message']:
         prev_message_id = rm_handler.menu_c()
-        prev_message_text = line_bot_api.get_message_content(
-            str(prev_message_id))
-        print(prev_message_text)
+        prev_message_text = requests.get(
+            'https://api-data.line.me/v2/bot/message/' + prev_message_id + '/content', headers=request_header)
+        print("prev_message" + prev_message_text)
     else:
         response_message = event.message.text + "でやんす"
         prev_message_id = event.message.id
-        prev_message_text = line_bot_api.get_message_content(
-            str(prev_message_id))
-        print(prev_message_text)
+        prev_message_text = requests.get(
+            'https://api-data.line.me/v2/bot/message/' + prev_message_id + '/content', headers=request_header)
+        print("prev_message" + prev_message_text)
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=response_message)
