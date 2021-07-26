35a3eb5ff53732b2c72a31f9864e36d029f92e2f
KentaMatsumura <photosynthesishope@gmail.com>
2021-07-26 20:11:17 +0900

    バグ修正

--- main.py
+++ main.py
@@ -60,28 +60,12 @@ def callback():
 def handle_message(event):
     rm_handler = rich_menu_handler(line_bot_api, event, scenario)
     prev_message_id = 0
-    prev_message_text = ""
     if event.message.text == scenario['rich_menu_scenario']['register_user']['trigger_message']:
         prev_message_id = rm_handler.menu_a()
-        print("id: " + prev_message_id)
-        print("request url: " + 'https://api-data.line.me/v2/bot/message/' +
-              prev_message_id + '/content')
-        print("request header: " + request_header)
-
-        prev_message_text = requests.get(
-            'https://api-data.line.me/v2/bot/message/' + prev_message_id + '/content', headers=request_header)
-        print("prev_message" + prev_message_text)
     elif event.message.text == scenario['rich_menu_scenario']['get_time_now']['trigger_message']:
         prev_message_id = rm_handler.menu_c()
-        prev_message_text = requests.get(
-            'https://api-data.line.me/v2/bot/message/' + prev_message_id + '/content', headers=request_header)
-        print("prev_message" + prev_message_text)
     else:
         response_message = event.message.text + "でやんす"
-        prev_message_id = event.message.id
-        prev_message_text = requests.get(
-            'https://api-data.line.me/v2/bot/message/' + prev_message_id + '/content', headers=request_header)
-        print("prev_message" + prev_message_text)
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=response_message)
